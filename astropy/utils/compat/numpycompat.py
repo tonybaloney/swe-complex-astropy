eb704eda29b9db2aa10b6c9279fa8bd6db642fe8
SWE-bench Complex <noreply@swebench.dev>
2026-03-19 21:51:19 +1100

    claude-opus-4.6 wily patch for astropy__astropy-19335
    Model: claude-opus-4.6
    Condition: wily
    Base commit: fc0ebade0574cdedfe323b4c49f9c07503942980
    Complexity status: ok
    Complexity metrics:
      ΔCC: +6.0
      ΔCogC: +1.0
      ΔLLOC: +18.0
      ΔMI: -2.341

--- astropy/utils/compat/numpycompat.py
+++ astropy/utils/compat/numpycompat.py
@@ -4,9 +4,12 @@ This is a collection of monkey patches and workarounds for bugs in
 earlier versions of Numpy.
 """
 
+import warnings
+
 import numpy as np
 
 from astropy.utils import minversion
+from astropy.utils.exceptions import AstropyDeprecationWarning
 
 __all__ = [
     "COPY_IF_NEEDED",
@@ -19,6 +22,8 @@ __all__ = [
     "NUMPY_LT_2_4",
     "NUMPY_LT_2_4_1",
     "NUMPY_LT_2_5",
+    "chararray",
+    "get_chararray",
 ]
 
 # TODO: It might also be nice to have aliases to these named for specific
@@ -36,3 +41,43 @@ NUMPY_LT_2_5 = not minversion(np, "2.5.0.dev0")
 
 
 COPY_IF_NEEDED = False if NUMPY_LT_2_0 else None
+
+
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+    from numpy.char import array as np_char_array
+    from numpy.char import chararray as np_chararray
+
+
+_deprecated_chararray_attributes = {
+    "capitalize", "center", "count", "decode", "encode", "endswith",
+    "expandtabs", "find", "index", "isalnum", "isalpha", "isdecimal",
+    "isdigit", "islower", "isnumeric", "isspace", "istitle", "isupper",
+    "join", "ljust", "lower", "lstrip", "replace", "rfind", "rindex",
+    "rjust", "rpartition", "rsplit", "rstrip", "split", "splitlines",
+    "startswith", "strip", "swapcase", "title", "translate", "upper",
+    "zfill"
+}  # fmt: skip
+
+
+class chararray(np_chararray):
+    """Version of np.char.chararray with deprecation warnings on special methods."""
+
+    def __getattribute__(self, name):
+        if name in _deprecated_chararray_attributes:
+            warnings.warn(
+                "chararray is deprecated, in future versions astropy will "
+                "return a normal array so the special chararray methods "
+                "(e.g., .rstrip()) will not be available. Use np.strings "
+                "functions instead.",
+                AstropyDeprecationWarning,
+            )
+        return super().__getattribute__(name)
+
+
+def get_chararray(obj, itemsize=None, copy=True, unicode=None, order=None):
+    """Get version of np.char.chararray that gives deprecation warnings on special methods."""
+    return np_char_array(
+        obj, itemsize=itemsize, copy=copy, unicode=unicode, order=order
+    ).view(chararray)
